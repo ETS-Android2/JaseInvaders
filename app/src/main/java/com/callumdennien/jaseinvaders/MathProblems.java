0a34def4a99c44e243cfed0b6fe87293f08e42c1
Callum Dennien <callum.callumdennien@my.jcu.edu.au>
2021-04-20 13:49:17 +1000

    Complete generate_addition() Test.

--- app/src/main/java/com/callumdennien/jaseinvaders/MathProblems.java
+++ app/src/main/java/com/callumdennien/jaseinvaders/MathProblems.java
@@ -1,9 +1,21 @@
 package com.callumdennien.jaseinvaders;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MathProblems {
+    Random random = new Random();
+
     public ArrayList<Integer> getAddition() {
-        return null;
+        int addendsOne = random.nextInt(100);
+        int addendsTwo = random.nextInt(100);
+        int answer = addendsOne + addendsTwo;
+
+        ArrayList<Integer> problem = new ArrayList<>();
+        problem.add(addendsOne);
+        problem.add(addendsTwo);
+        problem.add(answer);
+
+        return problem;
     }
 }
