9c80295f6d8a7b04d8926895c159c50d64a9c2c1
Callum Dennien <callum.callumdennien@my.jcu.edu.au>
2021-05-12 17:44:08 +1000

    Add listView to LeaderBoardActivity.

--- app/src/main/java/com/callumdennien/jaseinvaders/LeaderBoardActivity.java
+++ app/src/main/java/com/callumdennien/jaseinvaders/LeaderBoardActivity.java
@@ -2,29 +2,39 @@ package com.callumdennien.jaseinvaders;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.HashMap;
 
 public class LeaderBoardActivity extends AppCompatActivity {
     private SQLiteOpenHelper database;
+    private ListView scoreList;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader_board);
 
-        database = new SQLiteOpenHelper(this);
+        scoreList = findViewById(R.id.score_list);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        scoreList.setAdapter(adapter);
+
+        adapter.add("Callum: 1000 - Easy");
+        adapter.add("Bob: 1000 - Easy");
+
+
+//        database = new SQLiteOpenHelper(this);
 //        database.insertScore(database.getWritableDatabase(), "Callum", 24);
+//        HashMap<String, String> scores = database.queryScores();
 
-        HashMap<String, String> scores = database.queryScores();
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        // Stuff
-        return super.onOptionsItemSelected(item);
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
