11de5c4cf88c83814a003db11361531d6d13cb17
Callum Dennien <callum.callumdennien@my.jcu.edu.au>
2021-05-21 16:12:18 +1000

    add difficultyValue to Difficulty enum.

--- app/src/main/java/com/callumdennien/jaseinvaders/Difficulty.java
+++ app/src/main/java/com/callumdennien/jaseinvaders/Difficulty.java
@@ -1,13 +1,15 @@
 package com.callumdennien.jaseinvaders;
 
 public enum Difficulty {
-    EASY("EASY MODE"),
-    MEDIUM("MEDIUM MODE"),
-    HARD("HARD MODE");
+    EASY("EASY MODE", 10),
+    MEDIUM("MEDIUM MODE", 20),
+    HARD("HARD MODE", 30);
 
     public final String difficultyText;
+    public final int difficultyValue;
 
-    Difficulty(String difficultyText) {
+    Difficulty(String difficultyText, int difficultyValue) {
         this.difficultyText = difficultyText;
+        this.difficultyValue = difficultyValue;
     }
 }
